e1058bff81d23122397e00251fe033ae4207489c
Hashem Bader <hlbader@upei.ca>
2026-02-11 23:03:06 -0400

    render

--- web_app.py
+++ web_app.py
@@ -9,7 +9,7 @@ import joblib
 
 MODEL_FILENAME = "classification_logreg.joblib"
 MODEL_PATH = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), "..", "models", MODEL_FILENAME)
+    os.path.join(os.path.dirname(__file__), "models", MODEL_FILENAME)
 )
 
 app = Flask(
